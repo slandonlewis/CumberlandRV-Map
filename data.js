c167163ec5e974307daddafef63384e26e5a867f
Seth Lewis <97172419+slandonlewis@users.noreply.github.com>
2023-04-30 23:12:52 -0500

    added lot data

---
+++ data.js
@@ -0,0 +1,294 @@
+const database = {
+    lots: [
+        {
+            id: 1,
+            price: "$45 per night",
+            availability: "taken"
+        },
+        {
+            id: 2,
+            price: "$45 per night",
+            availability: "open"
+        },
+        {
+            id: 3,
+            price: "$45 per night",
+            availability: "taken"
+        },
+        {
+            id: 4,
+            price: "$45 per night",
+            availability: "taken"
+        },
+        {
+            id: 5,
+            price: "$850 + electric",
+            availability: "taken"
+        },
+        {
+            id: 6,
+            price: "$850 + electric",
+            availability: "taken"
+        },
+        {
+            id: 7,
+            price: "$850 + electric",
+            availability: "taken"
+        },
+        {
+            id: 8,
+            price: "$850 + electric",
+            availability: "taken"
+        },
+        {
+            id: 9,
+            price: "$850 + electric",
+            availability: "taken"
+        },
+        {
+            id: 10,
+            price: "$850 + electric",
+            availability: "taken"
+        },
+        {
+            id: 11,
+            price: "$850 + electric",
+            availability: "taken"
+        },
+        {
+            id: 12,
+            price: "$850 + electric",
+            availability: "taken"
+        },
+        {
+            id: 13,
+            price: "$850 + electric",
+            availability: "taken"
+        },
+        {
+            id: 14,
+            price: "$850 + electric",
+            availability: "taken"
+        },
+        {
+            id: 15,
+            price: "$850 + electric",
+            availability: "taken"
+        },
+        {
+            id: 16,
+            price: "$850 + electric",
+            availability: "taken"
+        },
+        {
+            id: 17,
+            price: "$850 + electric",
+            availability: "taken"
+        },
+        {
+            id: 18,
+            price: "$850 + electric",
+            availability: "taken"
+        },
+        {
+            id: 19,
+            price: "$850 + electric",
+            availability: "taken"
+        },
+        {
+            id: 20,
+            price: "$850 + electric",
+            availability: "taken"
+        },
+        {
+            id: 21,
+            price: "$850 + electric",
+            availability: "taken"
+        },
+        {
+            id: 22,
+            price: "$850 + electric",
+            availability: "taken"
+        },
+        {
+            id: 23,
+            price: "$850 + electric",
+            availability: "taken"
+        },
+        {
+            id: 24,
+            price: "$850 + electric",
+            availability: "taken"
+        },
+        {
+            id: 25,
+            price: "$850 + electric",
+            availability: "taken"
+        },
+        {
+            id: 26,
+            price: "$850 + electric",
+            availability: "taken"
+        },
+        {
+            id: 27,
+            price: "$550 + electric",
+            availability: "taken"
+        },
+        {
+            id: 28,
+            price: "$550 + electric",
+            availability: "taken"
+        },
+        {
+            id: 29,
+            price: "$550 + electric",
+            availability: "taken"
+        },
+        {
+            id: 30,
+            price: "$550 + electric",
+            availability: "taken"
+        },
+        {
+            id: 31,
+            price: "$550 + electric",
+            availability: "taken"
+        },
+        {
+            id: 32,
+            price: "$550 + electric",
+            availability: "taken"
+        },
+        {
+            id: 33,
+            price: "$550 + electric",
+            availability: "taken"
+        },
+        {
+            id: 34,
+            price: "$550 + electric",
+            availability: "taken"
+        },
+        {
+            id: 35,
+            price: "$550 + electric",
+            availability: "taken"
+        },
+        {
+            id: 36,
+            price: "$550 + electric",
+            availability: "taken"
+        },
+        {
+            id: 37,
+            price: "$550 + electric",
+            availability: "taken"
+        },
+        {
+            id: 38,
+            price: "$550 + electric",
+            availability: "taken"
+        },
+        {
+            id: 39,
+            price: "$550 + electric",
+            availability: "taken"
+        },
+        {
+            id: 40,
+            price: "$550 + electric",
+            availability: "taken"
+        },
+        {
+            id: 41,
+            price: "$700 + electric",
+            availability: "taken"
+        },
+        {
+            id: 42,
+            price: "$700 + electric",
+            availability: "taken"
+        },
+        {
+            id: 43,
+            price: "$700 + electric",
+            availability: "taken"
+        },
+        {
+            id: 44,
+            price: "$700 + electric",
+            availability: "taken"
+        },
+        {
+            id: 45,
+            price: "$700 + electric",
+            availability: "taken"
+        },
+        {
+            id: 46,
+            price: "$700 + electric",
+            availability: "taken"
+        },
+        {
+            id: 47,
+            price: "$700 + electric",
+            availability: "taken"
+        },
+        {
+            id: 48,
+            price: "$700 + electric",
+            availability: "taken"
+        },
+        {
+            id: 49,
+            price: "$700 + electric",
+            availability: "taken"
+        },
+        {
+            id: 50,
+            price: "$700 + electric",
+            availability: "taken"
+        },
+        {
+            id: 51,
+            price: "$700 + electric",
+            availability: "taken"
+        },
+        {
+            id: 52,
+            price: "$700 + electric",
+            availability: "taken"
+        },
+        {
+            id: 53,
+            price: "$700 + electric",
+            availability: "taken"
+        },
+        {
+            id: 54,
+            price: "$700 + electric",
+            availability: "taken"
+        },
+        {
+            id: 55,
+            price: "$700 + electric",
+            availability: "taken"
+        },
+        {
+            id: 56,
+            price: "$850 + electric",
+            availability: "taken"
+        },
+        {
+            id: 57,
+            price: "$850 + electric",
+            availability: "taken"
+        },
+        {
+            id: 58,
+            price: "$850 + electric",
+            availability: "taken"
+        }
+    ]
+}
